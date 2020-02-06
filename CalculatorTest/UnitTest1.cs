5481d334b0ea1f394fc1428b758ee8a0544cc9bf
Tobias Holm <201608355@post.au.dk>
2020-02-06 11:50:41 +0100

    LAST NET CORE TEST

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -1,62 +1,62 @@
-using Calculator;
-using NUnit.Framework;
+    using Calculator;
+    using NUnit.Framework;
 
-namespace CalculatorTest
-{
-    [TestFixture]
-    public class Test
+    namespace CalculatorTest
     {
-        
-        private CalculatorEx _cal;
-        [SetUp]
-        public void Init()
+        [TestFixture]
+        public class Test
         {
-            _cal = new CalculatorEx();
-        }
-        [TestCase(2,5,7)]
-        [TestCase(-2,5,3)]
-        [TestCase(200,5000,5200)]
-        public void Addition(double a, double b, double x)
-        {
-            Assert.That(_cal.addition(a,b),Is.EqualTo(x));
-        }
+            
+            private CalculatorEx _cal;
+            [SetUp]
+            public void Init()
+            {
+                _cal = new CalculatorEx();
+            }
+            [TestCase(2,5,7)]
+            [TestCase(-2,5,3)]
+            [TestCase(200,5000,5200)]
+            public void Addition(double a, double b, double x)
+            {
+                Assert.That(_cal.addition(a,b),Is.EqualTo(x));
+            }
 
-        [TestCase(2,5,-3)]
-        [TestCase(-2,5,-7)]
-        [TestCase(200,5000,-4800)]
+            [TestCase(2,5,-3)]
+            [TestCase(-2,5,-7)]
+            [TestCase(200,5000,-4800)]
 
-        public void Subtraction(double a, double b, double x)
-        {
-            Assert.That(_cal.subtract(a,b).Equals(x));
-        }
-        [TestCase(2,5,10)]
-        [TestCase(-2,5,-10)]
-        [TestCase(200,5000,1000000)]
-        public void Multiply(double a, double b, double x)
-        {
-            Assert.That(_cal.multiply(a,b),Is.EqualTo(x));
-        }
+            public void Subtraction(double a, double b, double x)
+            {
+                Assert.That(_cal.subtract(a,b).Equals(x));
+            }
+            [TestCase(2,5,10)]
+            [TestCase(-2,5,-10)]
+            [TestCase(200,5000,1000000)]
+            public void Multiply(double a, double b, double x)
+            {
+                Assert.That(_cal.multiply(a,b),Is.EqualTo(x));
+            }
 
-        [TestCase(2, 3, 8)]
-        [TestCase(2, 5, 32)]
-        [TestCase(3, 3, 27)]
-        public void Power(double a, double b, double x)
-        {
-            Assert.That(_cal.power(a,b).Equals(x));
-        }
-        [TestCase(10,5,2)]
-        [TestCase(100,20,5)]
-        [TestCase(5000,200,25)]
-        public void Div(double a, double b, double x)
-        {
-            Assert.That(_cal.div(a,b),Is.EqualTo(x));
-        }
-        [TestCase(5,2,1)]
-        [TestCase(8,4,0)]
-        [TestCase(200,5000,200)]
-        public void Mod(double a, double b, double x)
-        {
-            Assert.That(_cal.mod(a,b),Is.EqualTo(x));
+            [TestCase(2, 3, 8)]
+            [TestCase(2, 5, 32)]
+            [TestCase(3, 3, 27)]
+            public void Power(double a, double b, double x)
+            {
+                Assert.That(_cal.power(a,b).Equals(x));
+            }
+            [TestCase(10,5,2)]
+            [TestCase(100,20,5)]
+            [TestCase(5000,200,25)]
+            public void Div(double a, double b, double x)
+            {
+                Assert.That(_cal.div(a,b),Is.EqualTo(x));
+            }
+            [TestCase(5,2,1)]
+            [TestCase(8,4,0)]
+            [TestCase(200,5000,200)]
+            public void Mod(double a, double b, double x)
+            {
+                Assert.That(_cal.mod(a,b),Is.EqualTo(x));
+            }
         }
     }
-}
